387461bca6fb009cd0817a489bcad9e0a8cae7ef
user <user@users-MacBook-Air.local>
2019-02-15 16:56:05 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,8 @@
 
 
+
+/*
 console.log("Up and running!");
-
-
-
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
@@ -12,4 +11,32 @@ var cardFour = "king";
 
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardThree);
+console.log("User flipped " + cardTwo);
+
+*/
+
+
+
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");	
+	} else {
+		alert("Sorry try again.");
+	}
+}
+
+
+
+
 
