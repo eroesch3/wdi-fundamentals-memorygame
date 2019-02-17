716e0b406ffd88530d9a152854ff3b12c47a59e5
user <user@users-MacBook-Air.local>
2019-02-17 18:18:18 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -13,28 +13,99 @@ console.log("User flipped " + cardOne);
 console.log("User flipped " + cardThree);
 console.log("User flipped " + cardTwo);
 
+
 */
 
 
-
 var cards = ["queen", "queen", "king", "king"];
-
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
+var checkForMatch = function() {
+	if (cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+};
 
-if (cardsInPlay.length === 2){
+};
+
+
+
+
+
+
+
+var flipCard = function(cardId) {
+
+	console.log("User flipped " + cards[cardId]); 
+
+	cardsInPlay.push(cards[cardId]);
+
+	checkForMatch();
+};
+
+
+flipCard(2);
+flipCard(0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+
+
+
+
+
+	var cardOne = cards[0];
+	var cardTwo = cards[2];
+
+	cardsInPlay.push(cardOne);
+	cardsInPlay.push(cardTwo);
+
+	if (cardsInPlay.length === 2){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");	
 	} else {
 		alert("Sorry try again.");
 	}
 }
+
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
